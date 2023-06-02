4a3c68908e36ee9fd57995d8ce0bda08fbfd9e90
snjung <jung.jena@gmx.de>
2023-06-02 13:45:13 +0200

    Add buttons for open/close, introduced basic logic

--- Arduino/blastgate_control/blastgate_control.ino
+++ Arduino/blastgate_control/blastgate_control.ino
@@ -1,13 +1,34 @@
 #include <Blastgate.h>
+#include <JC_Button.h>
 
-Blastgate blastgate(8, 3, 0, 180); 
+Blastgate blastgateBandsaw(8, 3, 0, 180); 
+Blastgate blastgatePlaner(12, 5, 0, 180); 
+Blastgate blastgateTablesaw(13, 6, 0, 180); 
+int PinVacControl = 7;
+
+Button buttonBandsaw(0);
+Button buttonPlaner(1);
+Button buttonTablesaw(2);
 
 void setup() {
-  //serial.begin(9600);
-  blastgate.init();
+  serial.begin(9600);
+  blastgateBandsaw.init();
+  blastgatePlaner.init();
+  blastgateTablesaw.init();
+  buttonBandsaw.begin();
+  buttonPlaner.begin();
+  buttonBandsaw.begin();  
 }
 
 void loop() {
-  blastgate.openGate();
+  buttonPlaner.read();
+  if (buttonPlaner.wasReleased()) {
+    if(blastgatePlaner.getState){
+      blastgatePlaner.closeGate();
+    }
+    else{
+      blastgatePlaner.openGate();
+    }
+  }
 }
 
